a33cf70ef71ffd67d0218d5d0766dba31f89b747
Viper007Bond <Viper007Bond@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-12-16 01:03:29 +0000

    v1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/global-plugin-update-notice/trunk@26678 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ global-plugin-update-notice.php
@@ -0,0 +1,93 @@
+<?php /*
+
+**************************************************************************
+
+Plugin Name:  Global Plugin Update Notice
+Plugin URI:   http://wordpress.org/extend/plugins/global-plugin-update-notice/
+Version:      1.0.0
+Description:  When a new version of an activated plugin is available for download from WordPress.org, you will recieve a notice on all admin pages. No more having to check the plugins page!
+Author:       Viper007Bond
+Author URI:   http://www.viper007bond.com/
+
+**************************************************************************/
+
+class GlobalPluginUpdateNotice {
+
+	// Plugin initialization
+	function GlobalPluginUpdateNotice() {
+		if ( !is_admin() || !current_user_can('activate_plugins') ) return;
+
+		add_action( 'admin_head', array(&$this, 'CheckPlugins') ); // admin_head runs at just the right time (init is too early)
+		add_action( 'admin_notices', array(&$this, 'MaybeDisplayUpdateMessage'), 5 );
+	}
+
+
+	// Make wp_update_plugins() run on all admin pages
+	function CheckPlugins() {
+		// Make sure this version of WordPress is new enough to work with this plugin.
+		if ( !function_exists('wp_update_plugins') ) {
+			add_action( 'admin_notices', array(&$this, 'WordPressTooOld') );
+			return;
+		}
+
+		// Call the update checker
+		wp_update_plugins();
+	}
+
+
+	// Displays a message that this version of WordPress is too old
+	function WordPressTooOld() {
+		echo "	<div class='updated'><p>Your version of WordPress is too old for <strong>Global Plugin Update Notice</strong> to work. <a href='http://wordpress.org/download/'>Please update!</a></p></div>\n";
+	}
+
+
+	// Displays a message that plugin updates are available if they are
+	function MaybeDisplayUpdateMessage() {
+		$current = get_option( 'update_plugins' );
+
+		if ( empty( $current->response ) ) return; // No plugin updates available
+
+		// Since the message can get spammy, only display activated plugins
+		$active_plugins = get_option('active_plugins');
+		if ( empty($active_plugins) || !is_array($active_plugins) ) return;
+
+		$plugins = get_plugins();
+
+		$updatelist = array();
+
+		foreach ( $current->response as $plugin_file => $update_data ) {
+			// Make sure the plugin data is known and that it's activated
+			if ( empty( $plugins[$plugin_file] ) || !in_array( $plugin_file, $active_plugins ) ) continue;
+
+			// Make syre there is something to display
+			if ( empty($plugins[$plugin_file]['Name']) ) $plugins[$plugin_file]['Name'] = $plugin_file;
+
+			$updatelist[$plugin_file] = array(
+				'name' => $plugins[$plugin_file]['Name'],
+				'url' => $update_data->url,
+				'version' => $update_data->new_version,
+			);
+		}
+
+		if ( empty($updatelist) ) return; // There are plugins that need updating, but none of them are activated
+
+
+		echo '	<div class="plugin-update">';
+
+		if ( 1 == count($updatelist) ) {
+			printf( __('There is a new version of %s available. <a href="%s">Download version %s here</a>.'), $updatelist[0]['name'], $updatelist[0]['url'], $updatelist[0]['version'] );
+		} else {
+			foreach ( $updatelist as $item ) {
+				$temp[] = '<a href="' . $item['url'] . '">' . $item['name'] . ' ' . $item['version'] . '</a>';
+			}
+			printf( 'There are new versions available of the following plugins: %s', implode( ', ', $temp ) );
+		}
+
+		echo "</div>\n";
+	}
+}
+
+// Start this plugin once all other files and plugins are fully loaded
+add_action( 'plugins_loaded', create_function( '', 'global $GlobalPluginUpdateNotice; $GlobalPluginUpdateNotice = new GlobalPluginUpdateNotice();' ) );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== Global Plugin Update Notice ===
+Contributors: Viper007Bond
+Donate link: http://www.viper007bond.com/donate/
+Tags: admin, plugins, update, notice, warning
+Requires at least: 2.3
+Stable tag: trunk
+
+When a new version of an activated plugin is available for download from WordPress.org, you will recieve a notice on all admin pages. No more having to check the plugins page!
+
+== Description ==
+
+If you're like me, you want to make sure to always be running the latest version of all of the plugins that you have installed and activated. The problem though is that you have to keep checking your plugins page to see if any have updates available. Even worse, if you have a ton of plugins uploaded to your blog, that list is rather long.
+
+Enter Global Plugin Update Notice. When a new version of an activated plugin is available for download, you will recieve a notice on all admin pages much like you do when a new version of WordPress is available. This saves you time and makes sure your blog is secure and up to date.
+
+== Installation ==
+
+###Upgrading From A Previous Version###
+
+To upgrade from a previous version of this plugin, delete the entire folder and files from the previous version of the plugin and then follow the installation instructions below.
+
+###Installing The Plugin###
+
+Extract all files from the ZIP file, making sure to keep the file structure intact, and then upload the plugin's folder to `/wp-content/plugins/`.
+
+This should result in the following file structure:
+
+`- wp-content
+    - plugins
+        - global-plugin-update-notice
+            | global-plugin-update-notice.php
+            | readme.txt
+            | screenshot-1.png`
+
+Then just visit your admin area and activate the plugin.
+
+**See Also:** ["Installing Plugins" article on the WP Codex](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins)
+
+== ChangeLog ==
+
+**Version 1.0.0**
+
+* Initial release.
